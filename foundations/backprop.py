fb6488471f1b2fcc965cb73d4fa553fca40d4610
Hritik Kumar <88363539+Hritikkumar51@users.noreply.github.com>
2026-05-23 20:56:40 +0530

    Update Backpropagation (Build a Neural Net 2/5)

--- foundations/backprop.py
+++ foundations/backprop.py
@@ -14,16 +14,25 @@ class Solution:
         # Loss: L = 0.5 * (y_hat - y_true)^2
         # Return: (dL_dw rounded to 5 decimals, dL_db rounded to 5 decimals)
 
-        z=0.0
-        for i in range(len(x)):
-            z+=np.dot(x[i],w[i])
-        z+=b # adding bias
+        ###########My solution below #########
+        # z=0.0
+        # for i in range(len(x)):
+        #     z+=np.dot(x[i],w[i])
+        # z+=b # adding bias
 
-        y_hat=1/(1+np.exp(-z)) #sigmoid calculation
+        # y_hat=1/(1+np.exp(-z)) #sigmoid calculation
 
-        # L=0.5*((y_hat-y_true)**2)
+        # # L=0.5*((y_hat-y_true)**2)
 
-        grad_L_w=np.round((y_hat-y_true)*y_hat*(1-y_hat)*x,5) ##gradient calculation w.r.t w
-        grad_L_b=np.round(float(y_hat-y_true)*y_hat*(1-y_hat),5) ##gradient calculation w.r.t b but it needs to be in float type
+        # grad_L_w=np.round((y_hat-y_true)*y_hat*(1-y_hat)*x,5) ##gradient calculation w.r.t w
+        # grad_L_b=np.round(float(y_hat-y_true)*y_hat*(1-y_hat),5) ##gradient calculation w.r.t b but it needs to be in float type
 
-        return grad_L_w,grad_L_b
+        # return grad_L_w,grad_L_b
+
+
+        ########## Another simple solution ##########
+        z=np.dot(x,w)+b
+        y_hat=1/(1+np.exp(-z))
+        dL_dw=np.round((y_hat-y_true)*y_hat*(1-y_hat)*x,5)
+        dL_db=np.round(float((y_hat-y_true)*y_hat*(1-y_hat)),5)
+        return dL_dw,dL_db
